bf6a93d6c7e553e678730501db7b53f705eb1249
ParkSergey <pakush1826@gmail.com>
2025-04-22 21:20:30 +0900

    fix

--- file.js
+++ file.js
@@ -9,4 +9,13 @@ var swiper = new Swiper('.mySwiper', {
   },
   slidesPerView: 'auto',
 });
-console.log('hello');
+const button = document.querySelector('.hidden-button');
+button.onclick = showContent(){
+  if (content.style.display === 'none' || content.style.display === '') {
+        content.style.display = 'block'; // Показываем контент
+        button.textContent = 'Скрыть контент'; // Меняем текст кнопки
+      } else {
+        content.style.display = 'none'; // Скрываем контент
+        button.textContent = 'Показать контент'; // Меняем текст кнопки
+      }
+}

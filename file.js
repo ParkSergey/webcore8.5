31331059ae180113cab56183afcf396f72239260
ParkSergey <pakush1826@gmail.com>
2025-04-24 19:18:44 +0900

    swiper

--- file.js
+++ file.js
@@ -1,21 +1,11 @@
-var swiper = new Swiper('.mySwiper', {
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
-  pagination: {
-    el: '.swiper-pagination',
-    clickable: true,
-  },
-  slidesPerView: 'auto',
-});
-const button = document.querySelector('.hidden-button');
-button.onclick = showContent(){
-  if (content.style.display === 'none' || content.style.display === '') {
-        content.style.display = 'block'; // Показываем контент
-        button.textContent = 'Скрыть контент'; // Меняем текст кнопки
-      } else {
-        content.style.display = 'none'; // Скрываем контент
-        button.textContent = 'Показать контент'; // Меняем текст кнопки
-      }
-}
+ar swiper = new Swiper('.mySwiper', {
+   navigation: {
+     nextEl: '.swiper-button-next',
+     prevEl: '.swiper-button-prev',
+   },
+   pagination: {
+     el: '.swiper-pagination',
+     clickable: true,
+   },
+   slidesPerView: "auto",
+ });
